0d05f7d02df21c41758e8179763004f6097fa3fe
Same <same.haschemi@hotmail.de>
2020-08-18 16:33:04 +0200

    Endkunde View
    Endkunde View

--- SE2_CarLook_Ltd/src/main/java/utils/Views.java
+++ SE2_CarLook_Ltd/src/main/java/utils/Views.java
@@ -4,4 +4,5 @@ public class Views {
 
     public final static String LoginView = "LoginView";
     public final static String MainView = "MainView";
+    public final static String EndKundeView = "EndKundeView"; 
 }
